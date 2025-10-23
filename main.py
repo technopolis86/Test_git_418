2adf70e1713e1422a636ab1dab50db1efea4d453
Maxim <dimakss@mail.ru>
2025-10-23 12:53:03 +0500

    1 comit

---
+++ main.py
@@ -0,0 +1,16 @@
+print('PyCharm')
+
+
+# 1.Скачать и установить на комп Git
+# 1.
+# 1. Создать в проекте локальный репозиторий
+    # VCS (version control system) - Create Git Repositoriy
+# 1.
+# 1.
+# 1.
+# 1.
+# 1.
+# 1.
+# 1.
+# 1.
+# 1.
